351ca869af1265a677fed8e9fd18e1c676b28a83
陈睿 <chenrui920614@gmail.com>
2023-11-29 10:20:21 +0800

    init

---
+++ src/renderer/transform.js
@@ -0,0 +1,34 @@
+import { applyTransform, createSVGElement, mount } from "./utils";
+
+export function transform(type, context, ...params) {
+  const { group } = context;
+  applyTransform(group, `${type}(${params.join(', ')})`);
+}
+
+export function translate(context, cx, cy) {
+  transform('translate', context, cx, cy);
+}
+
+export function rotate(context, theta) {
+  transform('rotate', context, theta);
+}
+
+export function scale(context, sx, sy) {
+  transform('scale', context, sx, sy);
+}
+
+export function save(context) {
+  const { group } = context;
+
+  const newGroup = createSVGElement('g');
+
+  mount(group, newGroup);
+
+  context.group = newGroup;
+}
+
+export function restore(context) {
+  const { group } = context;
+
+  context.group = group.parentNode;
+}
